59dad238936a38110bd91cb82c39580a549c11df
SinghAnkitt <ankittsinghh023@gmail.com>
2026-06-30 11:47:31 +0530

    2DArray

---
+++ src/TwoDarray/p5.java
@@ -0,0 +1,28 @@
+package TwoDarray;
+// Print matrix in snake pattern
+import java.util.*;
+
+public class p5 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {2,8,3,4,7},
+                {7,2,1,6,3},
+                {5,5,4,1,4},
+                {3,1,8,2,6}
+        };
+        for(int i = 0; i < arr.length; i++){
+            if(i % 2 == 0){
+                for(int j = 0; j < arr[0].length; j++){
+                    System.out.print(arr[i][j] +" ");
+                }
+                System.out.println();
+            }
+            else{
+                for(int k = arr[0].length-1; k >= 0; k--){
+                    System.out.print(arr[i][k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
